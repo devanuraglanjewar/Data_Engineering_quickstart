8fd0986d0354399910c81aef8b8acfca4fccbe35
Anurag Lanjewar <85513169+devanuraglanjewar@users.noreply.github.com>
2025-02-22 18:22:47 +0000

    1 error solve

--- py_insert.py
+++ py_insert.py
@@ -6,21 +6,26 @@ from dotenv import load_dotenv
 from cryptography.hazmat.primitives import serialization
 
 load_dotenv()
+
+
 logging.basicConfig(level=logging.WARN)
 snowflake.connector.paramstyle='qmark'
 
 
 def connect_snow():
-    private_key =  "-----BEGIN PRIVATE KEY-----\n" + os.getenv("PRIVATE_KEY") + "\n-----END PRIVATE KEY-----\n"
+    private_key =  "\n-----BEGIN PRIVATE KEY-----\n" + os.getenv("PRIVATE_KEY") + "\n-----END PRIVATE KEY-----\n"
     private_key = private_key.encode() 
+
     p_key = serialization.load_pem_private_key(
-       private_key,        
-       password=None
+        private_key,
+        password=None
     )
+
     pkb = p_key.private_bytes(
         encoding=serialization.Encoding.DER,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption())
+        encryption_algorithm=serialization.NoEncryption()
+    )
 
     return snowflake.connector.connect(
         account= os.getenv("SNOWFLAKE_ACCOUNT"),
